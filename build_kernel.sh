717a81bedae1ade231e420427174528691ffdc7b
jt1134 <jt1134@gmail.com>
2011-11-29 20:06:12 -0600

    build fixes

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "***** Building : $CONFIG *****"
-make clean mrproper > /dev/null 2>&1
+make clean mrproper
 
-make ARCH=arm charge_defconfig 1>/dev/null 2>"$WORK"/errlog.txt
-make -j8 ARCH=arm HOSTCFLAGS="-g -O3" #1>"$WORK"/stdlog.txt 2>>"$WORK"/errlog.txt
+make ARCH=arm charge_defconfig
+make -j8 ARCH=arm HOSTCFLAGS="-g -O3"
 
 cp arch/arm/boot/zImage ../../../device/samsung/charge/kernel
 
