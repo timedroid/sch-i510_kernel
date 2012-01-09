b0acc16dcb583725966de0e2a7a6b14f076d0886
Matthew Grochowalski <matt@gwalski.com>
2012-01-08 19:44:47 -0500

    Started modifying build script to build modules + work with mtd

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,56 +1,31 @@
 #!/bin/bash
 
 # setup
-WORK=`pwd`
 DATE=$(date +%m%d)
+HOSTCC="ccache gcc"
+CROSS_COMPILE="ccache arm-linux-gnueabi-"
+MAKE_FLAGS=(-j6 HOSTCC="$HOSTCC" CROSS_COMPILE="$CROSS_COMPILE" ARCH=arm TARGET_PRODUCT=SCH-I510)
+KERNEL_MODULES="crypto/ansi_cprng.ko drivers/bluetooth/bthid/bthid.ko drivers/cdma_dpram/dpram.ko drivers/ltespi/ltespi.ko drivers/misc/samsung_modemctl/modemctl/modemctl.ko drivers/misc/samsung_modemctl/onedram/onedram.ko drivers/misc/samsung_modemctl/svnet/svnet.ko drivers/misc/vibetonz/vibrator.ko drivers/scsi/scsi_wait_scan.ko drivers/wtlfota_dpram/wtlfota_dpram.ko drivers/net/wireless/bcm4329/dhd.ko"
 
 # execution!
-cd ..
-
-# check for voodoo/non-voodoo
-if [ "$1" != "V" ]; then
-	CONFIG="novoodoo"
-	cd charge_initramfs
-	git checkout gingerbread
-	mv .git ../
-else
-	CONFIG="voodoo"
-	cd charge_initramfs
-	git checkout gingerbread-voodoo
-	mv .git ../
-fi
 
 # build the kernel
-cd $WORK
-echo "***** Building : $CONFIG *****"
 make clean mrproper > /dev/null 2>&1
 rm -f update/*.zip update/kernel_update/zImage
 
-make ARCH=arm charge_defconfig 1>/dev/null 2>"$WORK"/errlog.txt
-make -j6 CROSS_COMPILE=/opt/toolchains/arm-2010q1/bin/arm-none-linux-gnueabi- ARCH=arm HOSTCFLAGS="-g -O3" 1>"$WORK"/stdlog.txt 2>>"$WORK"/errlog.txt
-if [ $? != 0 ]; then
-		echo -e "FAIL!\n"
-		cd ..
-		mv .git charge_initramfs/
-		exit 1
-	else
-		echo -e "Success!\n"
-		rm -f "$WORK"/*log.txt
-fi
+make ARCH=arm charge_defconfig || exit 1
+make -j6 HOSTCC="$HOSTCC" CROSS_COMPILE="$CROSS_COMPILE" ARCH=arm TARGET_PRODUCT=SCH-I510 modules || exit 1
+cp $KERNEL_MODULES ../charge_initramfs/lib/modules || exit 1
+make -j6 HOSTCC="$HOSTCC" CROSS_COMPILE="$CROSS_COMPILE" ARCH=arm TARGET_PRODUCT=SCH-I510 zImage  || exit 1
 
 cp arch/arm/boot/zImage update/kernel_update/zImage
-if [ $CONFIG == novoodoo ]; then
-	cp arch/arm/boot/zImage recovery.bin
-	tar -c recovery.bin > "$DATE"_charge_recovery.tar
-	md5sum -t "$DATE"_charge_recovery.tar >> "$DATE"_charge_recovery.tar
-	mv "$DATE"_charge_recovery.tar "$DATE"_charge_recovery.tar.md5
-fi
+
+cp arch/arm/boot/zImage recovery.bin
+tar -c recovery.bin > "$DATE"_charge_recovery.tar
+md5sum -t "$DATE"_charge_recovery.tar >> "$DATE"_charge_recovery.tar
+mv "$DATE"_charge_recovery.tar "$DATE"_charge_recovery.tar.md5
+
 cd update
 zip -r -q kernel_update.zip .
-mv kernel_update.zip ../"$DATE"_charge_"$CONFIG".zip
-cd ../../
-mv .git charge_initramfs/
-cd $WORK
-echo -e "***** Successfully compiled: $CONFIG *****\n"
-
-if [ "$1" == "A" ]; then ./build_kernel.sh V; fi
+mv kernel_update.zip ../"$DATE"_charge_mtd.zip
+echo -e "***** Successfully compiled *****\n"
