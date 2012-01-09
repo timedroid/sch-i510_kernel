ac4fa20f8c71e592dc567a97ccf48b3e5c7c958b
Matthew Grochowalski <matt@gwalski.com>
2012-01-08 22:39:50 -0500

    More build script modifications

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
+checkfail(){ 
+	if [[ ! ${PIPESTATUS[0]} ]] 
+	then 
+		echo $1 failed
+		exit 1
+	else
+		echo $1 succeeded
+	fi
+}
+
 # setup
 DATE=$(date +%m%d)
 HOSTCC="ccache gcc"
-CROSS_COMPILE="ccache arm-linux-gnueabi-"
-MAKE_FLAGS=(-j6 HOSTCC="$HOSTCC" CROSS_COMPILE="$CROSS_COMPILE" ARCH=arm TARGET_PRODUCT=SCH-I510)
-KERNEL_MODULES="crypto/ansi_cprng.ko drivers/bluetooth/bthid/bthid.ko drivers/cdma_dpram/dpram.ko drivers/ltespi/ltespi.ko drivers/misc/samsung_modemctl/modemctl/modemctl.ko drivers/misc/samsung_modemctl/onedram/onedram.ko drivers/misc/samsung_modemctl/svnet/svnet.ko drivers/misc/vibetonz/vibrator.ko drivers/scsi/scsi_wait_scan.ko drivers/wtlfota_dpram/wtlfota_dpram.ko drivers/net/wireless/bcm4329/dhd.ko"
+CROSS_COMPILE="ccache /opt/toolchains/arm-2010q1/bin/arm-none-eabi-"
+KERNEL_MODULES="crypto/ansi_cprng.ko drivers/bluetooth/bthid/bthid.ko drivers/cdma_dpram/dpram.ko drivers/ltespi/ltespi.ko drivers/misc/samsung_modemctl/modemctl/modemctl.ko drivers/misc/samsung_modemctl/onedram/onedram.ko drivers/misc/samsung_modemctl/svnet/svnet.ko drivers/misc/vibetonz/vibrator.ko drivers/scsi/scsi_wait_scan.ko drivers/wtlfota_dpram/wtlfota_dpram.ko drivers/net/wireless/bcm4329/dhd.ko drivers/staging/android/logger.ko"
 
 # execution!
 
 # build the kernel
-make clean mrproper > /dev/null 2>&1
+exec 2>&1
+# make clean mrproper > /dev/null 2>&1
 rm -f update/*.zip update/kernel_update/zImage
 
-make ARCH=arm charge_defconfig || exit 1
-make -j6 HOSTCC="$HOSTCC" CROSS_COMPILE="$CROSS_COMPILE" ARCH=arm TARGET_PRODUCT=SCH-I510 modules || exit 1
-cp $KERNEL_MODULES ../charge_initramfs/lib/modules || exit 1
-make -j6 HOSTCC="$HOSTCC" CROSS_COMPILE="$CROSS_COMPILE" ARCH=arm TARGET_PRODUCT=SCH-I510 zImage  || exit 1
+make ARCH=arm charge_defconfig
+checkfail Configuration
+
+make -j6 HOSTCC="$HOSTCC" CROSS_COMPILE="$CROSS_COMPILE" ARCH=arm TARGET_PRODUCT=SCH-I510 modules | tee ../moduleBuild.txt
+checkfail "Module compilation"
+
+cp $KERNEL_MODULES ../charge_initramfs/lib/modules
+checkfail "Module copy"
+
+make -j6 HOSTCC="$HOSTCC" CROSS_COMPILE="$CROSS_COMPILE" ARCH=arm TARGET_PRODUCT=SCH-I510 zImage  | tee ../kernelBuild.txt
+checkfail "zImage compilation"
 
 cp arch/arm/boot/zImage update/kernel_update/zImage
 
