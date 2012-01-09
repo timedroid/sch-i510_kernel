e49e305453bf0e2a50ae17334b44aa32d101cb02
jt1134 <jt1134@gmail.com>
2012-01-09 00:44:22 -0600

    switch to bcm4329 wifi driver from aries 2.6.35 and install driver in device tree

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,6 +7,7 @@ make ARCH=arm charge_defconfig
 make -j8 ARCH=arm HOSTCFLAGS="-g -O3"
 
 cp arch/arm/boot/zImage ../../../device/samsung/charge/kernel
+cp drivers/net/wireless/bcm4329/bcm4329.ko ../../../device/samsung/charge
 
 echo -e "***** Successfully compiled: $CONFIG *****\n"
 
